0b1d8768c19b3ece459dce0c8bea55b61be3c462
Danny <danny200073@gmail.com>
2024-06-11 18:01:29 +0800

    JWT

---
+++ LookDaysAPI/Models/Service/JwtGenerator.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace LookDaysAPI.Models.Service
+{
+    public class JwtGenerator
+    {
+        private readonly IConfiguration _config;
+
+        public JwtGenerator(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public string GenerateJwtToken(string username, string role)
+        {
+            List<Claim> claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name, username),
+                    new Claim(ClaimTypes.Role, role),
+                };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _config.GetSection("JwtSettings:Key").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = new JwtSecurityToken(
+                issuer: _config.GetSection("JwtSettings:Issuer").Value,
+                audience: _config.GetSection("JwtSettings:Audience").Value,
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+                );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var jwt = tokenHandler.WriteToken(token);
+
+            return jwt;
+        }
+    }
+}
