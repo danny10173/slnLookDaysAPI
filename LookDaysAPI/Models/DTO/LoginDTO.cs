683521c7ac619e7b591b4200eef6a5ae84c2333e
Danny <danny200073@gmail.com>
2024-06-12 19:28:53 +0800

    userRepository

--- LookDaysAPI/Models/DTO/LoginDTO.cs
+++ LookDaysAPI/Models/DTO/LoginDTO.cs
@@ -2,6 +2,8 @@
 {
     public class LoginDTO
     {
+        public string Email { get; set; };
+
         public string Username { get; set; } = null!;
 
         public string Password { get; set; } = null!;
