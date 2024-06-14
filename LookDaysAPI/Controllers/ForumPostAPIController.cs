781b807d9f9df3778ff1e8d87a37a1d483661afe
Danny <danny200073@gmail.com>
2024-06-14 17:10:29 +0800

    get post by current user

--- LookDaysAPI/Controllers/ForumPostAPIController.cs
+++ LookDaysAPI/Controllers/ForumPostAPIController.cs
@@ -4,6 +4,11 @@ using LookDaysAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.OpenApi;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Authorization;
+using LookDaysAPI.DataAccess;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using LookDaysAPI.Models.DTO;
 
 namespace LookDaysAPI.Controllers
 {
@@ -11,6 +16,77 @@ namespace LookDaysAPI.Controllers
     [ApiController]
     public class ForumPostAPIController : ControllerBase
     {
+        private readonly LookdaysContext _context;
+        private UserRepository _userRepository;
+        public ForumPostAPIController(LookdaysContext context)
+        {
+            _context = context;
+            _userRepository = new UserRepository(_context);
+        }
+
+        private string decodeJWT(string jwt)
+        {
+            jwt = jwt.Replace("Bearer ", "");
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            JwtSecurityToken decodedToken = tokenHandler.ReadJwtToken(jwt);
+            string? username = decodedToken.Claims.FirstOrDefault(claim => claim.Type == ClaimTypes.Name)?.Value;
+
+            return username!;
+        }
+
+        [HttpGet("getByPoster"), Authorize(Roles = "user")]
+        public async Task<IActionResult> getPostByUser()
+        {
+            try
+            {
+                string? jwt = HttpContext.Request.Headers["Authorization"];
+                if (jwt == null || jwt == "") return BadRequest();
+                string username = decodeJWT(jwt);
+
+                if (username == null)
+                {
+                    return BadRequest();
+                }
+
+                User? user = await _userRepository.GetUserbyUsername(username);
+
+                if (user == null)
+                {
+                    return NotFound("使用者不存在");
+                }
+
+                //var post = await _context.ForumPosts.FindAsync(user.UserId);
+                var findpost = await _context.ForumPosts.Where(a => a.UserId == 10)
+                    .Select(
+                     fp => new ForumPostDTO()
+                     {
+                         UserId = fp.UserId,
+                         Username = fp.User.Username,
+                         PostId = fp.PostId,
+                         PostTitle = fp.PostTitle,
+                         PostTime = fp.PostTime,
+                         PostContent = fp.PostContent,
+                         Participants = fp.Participants
+                     }
+                    ).ToListAsync();
+                //if (findpost == null) return NotFound();
+                //var post = new ForumPostDTO()
+                //{
+                //    PostId = findpost.PostId,
+                //    PostTitle = findpost.PostTitle,
+                //    PostTime = findpost.PostTime,
+                //    UserId = findpost.UserId,
+                //    Username = findpost.User.Username,
+                //    PostContent = findpost.PostContent,
+                //    Participants = findpost.Participants
+                //};
+                return Ok(findpost);
+            }
+            catch (Exception)
+            {
+                return BadRequest("伺服器錯誤，請稍後再試");
+            }
+        }
     }
 
 
---
+++ LookDaysAPI/Models/DTO/ForumPostDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace LookDaysAPI.Models.DTO
+{
+    public class ForumPostDTO
+    {
+        public int PostId { get; set; }
+
+        public int? UserId { get; set; }
+
+        public string Username { get; set; } = null!;
+
+        public string? PostTitle { get; set; }
+
+        public DateTime? PostTime { get; set; }
+
+        public string? PostContent { get; set; }
+
+        public int Participants { get; set; }
+    }
+}
