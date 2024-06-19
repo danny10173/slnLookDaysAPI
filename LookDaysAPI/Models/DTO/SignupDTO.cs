8c3037fdc4193fe4bd0187ddd5d5c2cd90c7f1c7
Danny <danny200073@gmail.com>
2024-06-19 20:25:30 +0800

    hash done, ez

--- LookDaysAPI/Models/DTO/SignupDTO.cs
+++ LookDaysAPI/Models/DTO/SignupDTO.cs
@@ -2,6 +2,10 @@
 {
     public class SignupDTO
     {
+        public string Email { get; set; } = null;
 
+        public string Username { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
     }
 }
