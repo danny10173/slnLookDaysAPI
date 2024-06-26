f4ab4092657a09f5396052a9a900b8d2ffa4fd9e
Stanley <stanley05232930@gmail.com>
2024-06-26 16:07:19 +0800

    超級奇美拉

--- LookDaysAPI/Controllers/BookingStatusAPIController.cs
+++ LookDaysAPI/Controllers/BookingStatusAPIController.cs
@@ -5,6 +5,9 @@ using ReactApp1.Server.Models;
 using System.Linq;
 using System.Threading.Tasks;
 
+
+//UseIDToMerchantTradeNoController 暫時替代此方法
+
 namespace ReactApp1.Server.Controllers
 {
     [ApiController]
@@ -62,48 +65,7 @@ namespace ReactApp1.Server.Controllers
             return Ok();
         }
 
-        //[HttpPost("update-order-status")]
-        //public async Task<IActionResult> UpdateOrderStatus()
-        //{
-        //    //從 Session 獲取用戶 ID
-        //    var userIdString = HttpContext.Session.GetString("UserId");
-        //    if (string.IsNullOrEmpty(userIdString))
-        //    {
-        //        return BadRequest("UserId is required.");
-        //    }
 
-        //    // 將用戶 ID 轉換為整數
-        //    if (!int.TryParse(userIdString, out int userId))
-        //    {
-        //        return BadRequest("Invalid UserId format.");
-        //    }
-
-        //    // 獲取當前UserID的MerchantTradeNo不為空且BookingStatesId等於1的訂單
-        //    var bookingsWithStatus1 = await _context.Bookings
-        //        .Where(b => b.MerchantTradeNo != null && b.BookingStatesId == 1 && b.UserId == userId)
-        //        .ToListAsync();
-
-        //    // 獲取當前UserID所有MerchantTradeNo不為空 且 BookingStatesId等於4 且 BookingDate為最近的那一筆資料， 將該筆資料的BookingStatesId改為3
-        //    var latestBookingWithStatus4 = await _context.Bookings
-        //        .Where(b => b.MerchantTradeNo != null && b.BookingStatesId == 4 && b.UserId == userId)
-        //        .OrderByDescending(b => b.BookingDate)
-        //        .FirstOrDefaultAsync();
-
-        //    // 將符合條件的訂單的狀態改為3
-        //    foreach (var booking in bookingsWithStatus1)
-        //    {
-        //        booking.BookingStatesId = 3;
-        //    }
-        //    if (latestBookingWithStatus4 != null)
-        //    {
-        //        latestBookingWithStatus4.BookingStatesId = 3;
-        //    }
-
-        //    // 保存更改
-        //    await _context.SaveChangesAsync();
-
-        //    return Ok();
-        //}
 
     }
 }
